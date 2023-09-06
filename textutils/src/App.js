6ac5ef4ec16999288f6b243b09bb1f7d84867021
devanshV18 <devanshverma024@gmail.com>
2023-09-06 06:19:44 +0530

    Initial changes to app.css and app.js

--- textutils/src/App.js
+++ textutils/src/App.js
@@ -1,24 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+let name = "Devansh";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <nav>
+      <li>Home</li>
+      <li>About</li>
+      <li>Contact</li>
+
+    </nav>
+
+    <div className="container">
+      <h1>Hello {name}</h1>
+     <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias distinctio est facere similique ipsa libero ducimus neque quos beatae autem ratione explicabo aut ipsam porro praesentium, tempore hic officiis voluptatibus cum labore. Quibusdam, dolorum.</p>
     </div>
+    </>
+   
   );
 }
 
