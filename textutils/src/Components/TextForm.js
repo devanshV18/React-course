23f560a76f335a130b565b636eb5c7e16bcb190a
devanshV18 <devanshverma024@gmail.com>
2023-09-08 06:02:05 +0530

    Textutil app with 3 functionalities

--- textutils/src/Components/TextForm.js
+++ textutils/src/Components/TextForm.js
@@ -10,6 +10,12 @@ export default function TextForm(props) {
     setText(newText);
   }
 
+  const handleLowClick = () =>{
+    console.log("Uppercase was Clicked" + text);
+    let newText = text.toLocaleLowerCase();
+    setText(newText);
+  }
+
   const handleOnChange = (event) =>{
     console.log("On Change");
     setText(event.target.value);
@@ -18,15 +24,25 @@ export default function TextForm(props) {
   const [text, setText] = useState('Enter text Here'); 
   // setText("new Text"); updation ka correct way
   return (
- <div>
+    <>
+ <div className='container'>
   <h1>{props.heading}</h1>
   <div className="mb-3">
   
   <textarea className="form-control" value = {text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
 </div>
-<button className="btn btn-primary" onClick = {handleUpClick} >Convert to UpperCase</button>
+<button className="btn btn-primary mx-3" onClick = {handleUpClick} >Convert to UpperCase</button>
+<button className="btn btn-primary mx-3" onClick = {handleLowClick} >Convert to LowerCase</button>
 </div>
 
+<div className="container my-4">
+  <h1>Your Text Summary</h1>
+  <p>{text.split("").length} words and {text.length} characters</p>
+  <p>{0.008 * text.split("").length} Minutes Read </p>
+  <h2>Preview</h2>
+  <p>{text}</p>
+</div>
+</>
 
     
   )
